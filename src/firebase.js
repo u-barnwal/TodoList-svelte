565e8023a31c15be1935abd8f79a61494f916bb9
Utsav Barnwal <u.barnwal36@gmail.com>
2021-05-07 07:01:14 +0530

    update README

--- src/firebase.js
+++ src/firebase.js
@@ -1,13 +1,14 @@
 import firebase from "firebase/app";
 import "firebase/auth";
 import "firebase/firestore";
+
 var firebaseConfig = {
-  apiKey: "AIzaSyAcSlW1bttg9As0oIVHZmhk1vHZGN_LzC8",
-  authDomain: "todolist-svelte.firebaseapp.com",
-  projectId: "todolist-svelte",
-  storageBucket: "todolist-svelte.appspot.com",
-  messagingSenderId: "1096926524284",
-  appId: "1:1096926524284:web:6285a0747581bdc6c36af9",
+  apiKey: API_KEY,
+  authDomain: AUTH_DOMAIN,
+  projectId: PROJECT_ID,
+  storageBucket: STORAGE_BUCKET,
+  messagingSenderId: MESSAGING_SENDER_ID,
+  appId: APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
